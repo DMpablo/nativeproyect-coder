7cd6fc2b04a2890df00e795e48f725170dc9d325
DMpablo <phdemarco@gmail.com>
2022-09-07 23:33:57 -0300

    Components individuals OK

--- App.js
+++ App.js
@@ -3,13 +3,12 @@ import {
   Text,
   View,
   Button,
-  TextInput,
   SafeAreaView,
   FlatList,
   TouchableOpacity,
-  Modal 
 } from "react-native";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { AddTask, CustomModal } from "./components/index";
 
 const paletColors = {
   colorOne: "#EE6C4D",
@@ -20,50 +19,22 @@ const paletColors = {
 };
 
 const styles = StyleSheet.create({
-  containerLoading: {
-    flex: 1,
-    backgroundColor: paletColors.colorTwo,
-    alignItems: "center",
-    justifyContent: "center",
-  },
   container: {
     flex: 1,
-    backgroundColor: paletColors.colorFive,
-  },
-  inputContainer: {
-    marginTop: 50,
-    marginBottom: 20,
-    marginHorizontal: 20,
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  inputMultiContainer: {
-    flex: 1,
-    alignItems: "center",
-    marginTop: 50,
-    marginBottom: 20,
-    marginHorizontal: 20,
+    backgroundColor: paletColors.colorTwo,
   },
 
-  input: {
-    width: "75%",
-    borderBottomColor: paletColors.colorTree,
-    borderBottomWidth: 1,
-    fontSize: 20,
-    height: 40,
-    color: paletColors.colorTwo,
-  },
   container_item: {
-    backgroundColor: paletColors.colorFive,
+    backgroundColor: paletColors.colorTwo,
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
     borderRadius: 4,
     borderWidth: 1,
-    borderColor: paletColors.colorFor,
+    borderColor: paletColors.colorTwo,
     flex: 1,
     flexDirection: "row",
-    alignItems: "center",
+    alignitems: "center",
     justifyContent: "space-between",
     shadowColor: "#000",
     shadowOffset: {
@@ -72,49 +43,66 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.29,
     shadowRadius: 4.65,
-
     elevation: 7,
   },
   title: {
-    color: paletColors.colorTwo,
+    color: paletColors.colorFive,
     fontSize: 22,
   },
   item: {
     margin: 0,
   },
+  modalContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+    paddingVertical: 20,
+  },
+  modalTitle: {
+    fontSize: 16
+  },
+  modalMessageContainer : {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  modalMessage: {
+    fontSize: 14,
+  },
+  selectedTask: {
+    fontSize: 16,
+    color: '#000000',
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginHorizontal: 20,
+  },
+  button: {
+    // backgroundColor: paletColors.colorTree,
+    padding: 10,
+    // borderRadius: 10,
+  }
 });
-
-
 
 export default function App() {
   const [textInput, setTextInput] = useState("");
-  const [item, setItem] = useState([]); //para seghuir el curso
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedTask, setSelectedTask] = useState(null);
+  const [item, setItem] = useState([]);
 
-  const onHandleDeleteItem = (id) => {
+  const onHandleDeleteitem = (id) => {
     setItem(item.filter((item) => item.id !== id));
     setSelectedTask(null);
     setModalVisible(!modalVisible);
-  }
+  };
 
   const onHandleModal = (id) => {
     setModalVisible(!modalVisible);
-    setSelectedTask(item.find((item) => item.id === id))
-    console.warn(id);
-  }
-  
-  const Item = ({ item }) => (
-    <View style={styles.container_item}>
-      <Text style={styles.title}>{item.value}</Text>
-  
-      <TouchableOpacity style={styles.button} onPress={() => onHandleModal(item.id)}>
-          <Text style={styles.delete}>X</Text>
-        </TouchableOpacity>
-    </View>
-  );
-  
-  const renderItem = ({ item }) => <Item item={item} />;
+    setSelectedTask(item.find((item) => item.id === id));
+    // console.warn(id);
+  };
 
   const onHandleText = (e) => {
     setTextInput(e);
@@ -129,51 +117,65 @@ export default function App() {
       },
     ]);
     setTextInput("");
-    // console.log(data);
   };
 
+  const renderItem = ({ item }) => (
+    <View style={styles.container_item}>
+      <Text style={styles.title}>{item.value}</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => onHandleModal(item.id)}
+      >
+        <Text>X</Text>
+      </TouchableOpacity>
+    </View>
+  );
 
   return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          onChangeText={onHandleText}
-          value={textInput}
-        />
-        <Button title="add" onPress={addItem} />
-      </View>
-
-      <SafeAreaView style={styles.container}>
-        <FlatList
-          data={item}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-        />
-      </SafeAreaView>
-      <Modal animationType='slide' visible={modalVisible}>
+      <AddTask
+        item={item}
+        onChangeText={onHandleText}
+        placeholder="new task"
+        addItem={addItem}
+        selectionColor="#4A306D"
+        placeholderTextColor="#4A306D"
+        textButton=" + "
+        color="#4A306D"
+      />
+      <FlatList
+        data={item}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        showsVerticalScrollIndicator={false}
+      />
+      <CustomModal animationType="slide" visible={modalVisible}>
         <View style={styles.modalContainer}>
           <Text style={styles.modalTitle}>Detalle de la lista</Text>
         </View>
-        <View style={styles.modalMessageContainer}> 
-          <Text style={styles.modalMessage}>¿Estas seguro de que quieres eliminar?</Text>
+        <View style={styles.modalMessageContainer}>
+          <Text style={styles.modalMessage}>
+            ¿Estas seguro de que quieres eliminar?
+          </Text>
         </View>
-        <View style={styles.modalMessageContainer}> 
+        <View style={styles.modalMessageContainer}>
           <Text style={styles.selectedTask}>{selectedTask?.value}</Text>
         </View>
         <View style={styles.buttonContainer}>
-          <Button 
-            title='Eliminar'
-            onPress={() => onHandleDeleteItem(selectedTask?.id)}
-            color='#4A306D'
+          <Button
+            style={styles.button}
+            title="Eliminar"
+            onPress={() => onHandleDeleteitem(selectedTask?.id)}
+            // color="#4A306D"
           />
-          <Button 
-            title='Cancelar'
+          <Button
+            style={styles.button}
+            title="Cancelar"
             onPress={() => setModalVisible(!modalVisible)}
-            color='#cccccc'
+            // color="#cccccc"
           />
         </View>
-      </Modal>
+      </CustomModal>
     </View>
   );
 }
---
+++ components/index.js
@@ -0,0 +1,2 @@
+export { default as CustomModal } from '../components/modal/index.js'
+export { default as AddTask } from './add-tasks/index.js'
